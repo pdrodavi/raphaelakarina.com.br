fc8901bbc79e06a049221e75f6017417c9ab5805
Pedro Davi <pdrodavi@outlook.com>
2021-12-07 21:53:25 -0300

    feature

--- js/main.js
+++ js/main.js
@@ -99,12 +99,13 @@
         mm = parseInt(mm) + 1;
         mm = String(mm).padStart(2, '0');
     }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
+    //var timerdate = mm + '/' + dd + '/' + yyyy;
     // For demo preview end
     
 
     // Use this for real timer date
     /*  var timerdate = "2020/01/01"; */
+    var timerdate = "2022/04/30";
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
